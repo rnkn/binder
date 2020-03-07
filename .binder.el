1cc2d708a9499b5fda4aa811067c7b045a84be1b
William Rankin <code@william.bydasein.com>
2020-03-07 14:05:56 +1000

    Add a full binder project/tutorial

--- .binder.el
+++ .binder.el
@@ -4,29 +4,35 @@
 
 ((structure
   ("binder.el"
-   (status . "alpha")
-   (notes . "Seriously though, am I even gonna use this?"))
+   (status . "alpha"))
   ("README.md"
    (include)
    (status . "todo"))
-  ("incident.txt"
+  ("start-here.txt"
+   (notes . "This file is going by a pseudonym")
+   (status . "start-here")
    (include)
-   (notes . "Story-writing books are particularly concerned with an \"inciting incident\" so make sure yours has one!"))
-  ("foo.txt"
-   (status . "firstdraft")
-   (include . t)
-   (display . "beginning"))
-  ("bar.txt"
-   (status . "revised")
-   (include . t)
-   (display . "middle"))
-  ("baz.txt"
-   (notes . "You can have flaws, but wow 'em in the end and you've got a hit.")
-   (include)
-   (display . "end"))
+   (display . "foo"))
+  ("sidebar.txt"
+   (include . t))
+  ("navigating.txt"
+   (include . t))
+  ("stapling.txt"
+   (include . t))
+  ("stapling-2.txt"
+   (display . "editing stapled stuff")
+   (include))
+  ("notes.txt"
+   (notes . "Some notes about notes.\n\nNullam eu ante vel est convallis dignissim. Fusce suscipit, wisi nec facilisis facilisis, est dui fermentum leo, quis tempor ligula erat quis odio. Nunc porta vulputate tellus. Nunc rutrum turpis sed pede. Sed bibendum. Aliquam posuere. Nunc aliquet, augue nec adipiscing interdum, lacus tellus malesuada massa, quis varius mi purus non odio."))
+  ("more-notes.txt"
+   (notes . "Even more notes.\n\nEmbiggen this window with C-c C-l."))
   ("akai-ryu.jpg"
    (include)
    (status . "image")
-   (notes . "Dionaea muscipula \"Akai Ryu\", Japanese for \"Red Dragon\"\n~ Wikipedia")))
+   (notes . "The binder is not just for writing, but references too! Here's an image*\n\nDionaea muscipula \"Akai Ryu\", Japanese for \"Red Dragon\"\n~ Wikipedia\n\n* your Emacs may not support images :("))
+  ("dog.txt"
+   (notes . "ASCII art byt jgs"))
+  ("end.txt"
+   (display . "The End")))
  (default-mode . text-mode)
  (default-extension . "txt"))
