0e5cbb99f207e4a85ea397e85cf6503860271b5f
William Rankin <code@william.bydasein.com>
2020-03-09 17:52:35 +1000

    Move tutorial to subdirectory

--- .binder.el
+++ .binder.el
@@ -12,27 +12,27 @@
    (notes . "This file is going by a pseudonym")
    (status . "start-here")
    (include)
-   (display . "foo"))
-  ("sidebar.txt"
+   (display . "tutorial/foo"))
+  ("tutorial/sidebar.txt"
    (include . t))
-  ("navigating.txt"
+  ("tutorial/navigating.txt"
    (include . t))
-  ("stapling.txt"
+  ("tutorial/stapling.txt"
    (include . t))
-  ("stapling-2.txt"
-   (display . "editing stapled stuff")
+  ("tutorial/stapling-2.txt"
+   (display . "tutorial/editing stapled stuff")
    (include))
-  ("notes.txt"
+  ("tutorial/notes.txt"
    (notes . "Some notes about notes.\n\nNullam eu ante vel est convallis dignissim. Fusce suscipit, wisi nec facilisis facilisis, est dui fermentum leo, quis tempor ligula erat quis odio. Nunc porta vulputate tellus. Nunc rutrum turpis sed pede. Sed bibendum. Aliquam posuere. Nunc aliquet, augue nec adipiscing interdum, lacus tellus malesuada massa, quis varius mi purus non odio."))
-  ("more-notes.txt"
+  ("tutorial/more-notes.txt"
    (notes . "Even more notes.\n\nEmbiggen this window with C-c C-l."))
-  ("akai-ryu.jpg"
+  ("tutorial/akai-ryu.jpg"
    (include)
    (status . "image")
    (notes . "The binder is not just for writing, but references too! Here's an image*\n\nDionaea muscipula \"Akai Ryu\", Japanese for \"Red Dragon\"\n~ Wikipedia\n\n* your Emacs may not support images :("))
-  ("dog.txt"
-   (notes . "ASCII art byt jgs"))
-  ("end.txt"
-   (display . "The End")))
+  ("tutorial/dog.txt"
+   (notes . "ASCII art by jgs"))
+  ("tutorial/end.txt"
+   (display . "tutorial/The End")))
  (default-mode . text-mode)
  (default-extension . "txt"))
