681d27f2e5eef5cd26dbfa72d39ec594f89be496
Paul W. Rankin <rnkn@rnkn.xyz>
2020-06-26 12:32:31 +1000

    Update .binder.el to use tags

--- .binder.el
+++ .binder.el
@@ -3,32 +3,41 @@
 ;; probably shouldn't edit it.
 
 ((structure
-  ("binder.el")
+  ("binder.el"
+   (tags "code"))
   ("README.md")
   ("TODO.md")
   ("tutorial/start-here.txt"
    (notes . "This file is going by a pseudonym")
-   (status . "start-here")
+   (tags "start-here" "tutorial")
    (display . "tutorial/foo"))
   ("tutorial/sidebar.txt"
-   (include . t))
+   (include . t)
+   (tags "tutorial"))
   ("tutorial/navigating.txt"
-   (include . t))
+   (include . t)
+   (tags "tutorial"))
   ("tutorial/stapling.txt"
-   (include . t))
+   (include . t)
+   (tags "tutorial"))
   ("tutorial/stapling-2.txt"
-   (display . "tutorial/editing stapled stuff"))
+   (display . "tutorial/editing stapled stuff")
+   (tags "tutorial"))
   ("tutorial/notes.txt"
-   (notes . "Some notes about notes.\n\nNullam eu ante vel est convallis dignissim. Fusce suscipit, wisi nec facilisis facilisis, est dui fermentum leo, quis tempor ligula erat quis odio. Nunc porta vulputate tellus. Nunc rutrum turpis sed pede. Sed bibendum. Aliquam posuere. Nunc aliquet, augue nec adipiscing interdum, lacus tellus malesuada massa, quis varius mi purus non odio."))
+   (notes . "Some notes about notes.\n\nNullam eu ante vel est convallis dignissim. Fusce suscipit, wisi nec facilisis facilisis, est dui fermentum leo, quis tempor ligula erat quis odio. Nunc porta vulputate tellus. Nunc rutrum turpis sed pede. Sed bibendum. Aliquam posuere. Nunc aliquet, augue nec adipiscing interdum, lacus tellus malesuada massa, quis varius mi purus non odio.")
+   (tags "tutorial"))
   ("tutorial/more-notes.txt"
-   (notes . "Even more notes.\n\nEmbiggen this window with C-c C-l."))
+   (notes . "Even more notes.\n\nEmbiggen this window with C-c C-l.")
+   (tags "tutorial"))
   ("tutorial/akai-ryu.jpg"
    (display . "tutorial/akai-ryu.jpg")
-   (status . "image")
+   (tags "tutorial" "image")
    (notes . "The binder is not just for writing, but references too! Here's an image*\n\nDionaea muscipula \"Akai Ryu\", Japanese for \"Red Dragon\"\n~ Wikipedia\n\n* your Emacs may not support images :("))
   ("tutorial/dog.txt"
-   (notes . "ASCII art by jgs"))
+   (notes . "ASCII art by jgs")
+   (tags "tutorial"))
   ("tutorial/end.txt"
-   (display . "tutorial/The End")))
+   (display . "tutorial/The End")
+   (tags "tutorial")))
  (default-stapled-mode . text-mode)
  (default-extension . "txt"))
