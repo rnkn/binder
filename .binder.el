aa3c105a270eb433355e931b3e55f9b4389d10ee
Paul W. Rankin <rnkn@rnkn.xyz>
2020-07-20 14:17:46 +1000

    Update tutorial

--- .binder.el
+++ .binder.el
@@ -9,26 +9,30 @@
   ("TODO.md")
   ("tutorial/start-here.txt"
    (notes . "This file is going by a pseudonym")
-   (tags "start-here" "tutorial")
-   (display . "tutorial/foo"))
-  ("tutorial/sidebar.txt"
-   (include . t)
-   (tags "tutorial" "foo"))
+   (tags "start-here" "tutorial"))
   ("tutorial/navigating.txt"
    (include . t)
    (tags "tutorial" "foo" "bar"))
-  ("tutorial/stapling.txt"
+  ("tutorial/sidebar.txt"
    (include . t)
-   (tags "tutorial" "bar"))
-  ("tutorial/stapling-2.txt"
-   (display . "tutorial/editing stapled stuff")
+   (tags "tutorial"))
+  ("tutorial/marks.txt"
+   (include . t)
    (tags "tutorial"))
   ("tutorial/notes.txt"
    (notes . "Some notes about notes.\n\nNullam eu ante vel est convallis dignissim. Fusce suscipit, wisi nec facilisis facilisis, est dui fermentum leo, quis tempor ligula erat quis odio. Nunc porta vulputate tellus. Nunc rutrum turpis sed pede. Sed bibendum. Aliquam posuere. Nunc aliquet, augue nec adipiscing interdum, lacus tellus malesuada massa, quis varius mi purus non odio.")
    (tags "tutorial"))
-  ("tutorial/more-notes.txt"
-   (notes . "Even more notes.\n\nEmbiggen this window with C-c C-l.")
+  ("tutorial/tags.txt"
+   (tags "tutorial" "foo"))
+  ("tutorial/concat.txt"
+   (include . t)
+   (tags "tutorial" "bar"))
+  ("tutorial/concat-2.txt"
+   (display . "tutorial/concat-including")
    (tags "tutorial"))
+  ("tutorial/concat-3.txt"
+   (tags "tutorial")
+   (display . "tutorial/concat-editing"))
   ("tutorial/akai-ryu.jpg"
    (display . "tutorial/akai-ryu.jpg")
    (tags "tutorial" "image")
@@ -37,7 +41,4 @@
    (notes . "ASCII art by jgs")
    (tags "tutorial"))
   ("tutorial/end.txt"
-   (display . "tutorial/The End")
-   (tags "tutorial")))
- (default-extension . "txt")
- (default-concat-mode . text-mode))
+   (tags "tutorial"))))
