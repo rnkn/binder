6fd8a2d6caafa373936ca7a0ad44b6e1b9845e9c
Paul W. Rankin <rnkn@rnkn.xyz>
2020-09-25 19:39:39 +1000

    Add theme

---
+++ contrib/binder-theme.el
@@ -0,0 +1,17 @@
+;; To install this theme, copy or symlink it to your `user-emacs-directory'.
+
+(deftheme binder
+  "Binder theme, inspired by Scrivener.")
+
+(custom-theme-set-faces
+ 'binder
+ '(binder-sidebar
+   ((default (:height 0.9))
+    (((background light)) (:background "#f1f0f6"))
+    (((background dark)) (:background "#33323f"))))
+ '(binder-notes
+   ((default (:height 0.9))
+    (((background light)) (:background "#fefbe8"))
+    (((background dark)) (:foreground "dark goldenrod")))))
+
+(provide-theme 'binder)
