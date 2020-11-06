4f7f34b786c98b7fa8b5cd7408c9b455b8c60bc0
Paul W. Rankin <pwr@skeletons.cc>
2020-11-06 13:33:07 +1000

    Use binder-file-header var in binder-tutorial project file

--- binder-tutorial.el
+++ binder-tutorial.el
@@ -29,12 +29,9 @@
 (require 'binder)
 
 (defconst binder-tutorial-items
-  '((".binder.el" . "\
-;; -*- coding: utf-8; -*-
-;; Binder-Format-Version: 2
-;; This is a Binder project file. It is meant to be human-readable, but you
-;; probably shouldn't edit it.
-
+  (list
+   (cons ".binder.el"
+         (concat binder-file-header "\
 ((\"projects.txt\"
   (tags \"start-here\"))
  (\"sidebar.txt\"
@@ -55,8 +52,8 @@
   (display . \"concat-including\"))
  (\"concat-3.txt\"
   (display . \"concat-editing\"))
- (\"errors.txt\"))")
-    ("projects.txt" . "\
+ (\"errors.txt\"))"))
+   (cons "projects.txt" "\
 A project in Binder is a linear list of files in with associated
 information (i.e. their order, inclusion state, notes, tags). This
 information is stored in a .binder.el file in the top-level directory of
@@ -81,7 +78,7 @@ However when visiting file baz.txt, project B would take precedence.
 
 Most management of a project will happen via the sidebar. Type C-c ] to
 go to sidebar.txt.")
-    ("sidebar.txt" . "\
+   (cons "sidebar.txt" "\
 By default items in the sidebar will show:
 
     x * name        #tag1 #tag2
@@ -110,7 +107,7 @@ Binder will automatically write its current project file after a certain
 number of changes, but you can manually save a project by typing s.
 
 Type C-c ] to go to the next file.")
-    ("navigation.txt" . "\
+   (cons "navigation.txt" "\
 You can navigate through project items forward and backward with C-c ]
 and C-c [, which means Emacs will visit each file in the project list in
 succession.
@@ -123,14 +120,14 @@ The highlight in the sidebar lets you know where you are so you don't
 get lost.
 
 Type C-c ] to go to the next file.")
-    ("marks.txt" . "\
+   (cons "marks.txt" "\
 You can mark multiple project items in the sidebar by typing m. Unmark
 an item with u or unmark all items with U.
 
 Marking items allows you to perform actions on multiple items at once.
 
 Type C-c ] to go to the next file.")
-    ("notes.txt" . "\
+   (cons "notes.txt" "\
 Each item can have notes, which are stored in the project file. In the
 sidebar, type z to open this item's notes.
 
@@ -147,7 +144,7 @@ of a side-window). This command works as a toggle.
 A file's notes can be edited without opening the sidebar with C-c \".
 
 Type C-c ] to go to the next file.")
-    ("tags.txt" . "\
+   (cons "tags.txt" "\
 Each project item can have any number of arbitrary tags, which are
 prefixed with # by default.
 
@@ -169,7 +166,7 @@ considers those items.
 
 Type g to clear the tag filters and refresh the sidebar, then C-c ] to
 go to the next file.")
-    ("concat.txt" . "\
+   (cons "concat.txt" "\
 Notice that the tutorial items for navigating, sidebar and marking are
 marked with an x? These items are included when you concatenate the
 project.
@@ -178,7 +175,7 @@ In the sidebar, type c (or v) to open the concat view. Congratulations,
 you've just concatenated some writing!
 
 Type q to quit the concat view and C-c ] to go to the next file.")
-    ("concat-2.txt" . "\
+   (cons "concat-2.txt" "\
 By default, concatenating a project enables view-mode, because you're
 not actually looking at the original files.
 
@@ -192,7 +189,7 @@ Now, include the following items:
     concat-editing
 
 Now type c again.")
-    ("concat-3.txt" . "\
+   (cons "concat-3.txt" "\
 ==> Move the cursor here and type C-c RET. <==
 
 This is how you go from viewing a concatenated project as a whole back
@@ -200,7 +197,7 @@ to the original file for editing. The concat view is good for
 proofreading and creating a final export, but not editing.
 
 Type C-c ] to go to the next file.")
-    ("errors.txt" . "\
+   (cons "errors.txt" "\
 Binder caches project information to avoid contious reading and writing
 to disk. However this means errors may occur if a project is moved on
 disk while project information is cached. If you plan on moving a
